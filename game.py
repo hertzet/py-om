c3acfd13543332b0832ddf7e23e77416f5e6e2db
gato <leo@mis7vidas.com>
2022-05-28 15:17:14 -0300

    commit 28/5/22

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 # from ursina.shaders import normals_shader
 # from ursina.shaders import lit_with_shadows_shader
 from ursina.shaders import basic_lighting_shader
-
+Entity.default_shader = basic_lighting_shader
 # from ursina.shaders import matcap_shader - un re viaje
 
 app = Ursina()
@@ -16,37 +16,33 @@ camera.fov = 100
 
 editor_camera = EditorCamera(enabled=False, ignore_paused=True)
 
-MAP = Entity(model='e1m1.obj', texture='texturesheet.png', shader=basic_lighting_shader)
+MAP = Entity(model='e1m1.obj', texture='sheet133_166-6.png')
 MAP.collider = MeshCollider(MAP, center=(0, 0, 0))
-# Entity(model='plane', collider='box', scale=500, texture='grass', texture_scale=(4,4))
-player = FirstPersonController(model='cube', texture='2dassets/py.png', scale=(2, 2, 2), speed=20, shader=basic_lighting_shader)
+player = FirstPersonController(model='cube', texture='2dassets/py.png', scale=(2, 2, 2), speed=20)
 player.collider = BoxCollider(player, Vec3(2, 2, 2))
-Text(text='Weapon: Gun1', position=(0, -.40, 0))
+Text(text='Weapon: Gun1', position=(0, -.45, 0))
 
-gun1 = Entity(model='cube', parent=camera, position=(0, -.25, .25), scale=(.1, .1, .3), origin_z=-.5, color=color.red,
-              on_cooldown=False, shader=basic_lighting_shader)
-# gun = Entity(model='gun.fbx', parent=camera, color=color.red, on_cooldown=False, world_scale=1)
-# position=(.5, -.25, .25), scale=(.3, .2, 1), origin_z=-.5
-gun1.muzzle_flash = Entity(parent=gun1, z=1, world_scale=.5, model='quad', color=color.yellow, enabled=False)
+gun1 = Entity(model='3dassets/assaultrifle1', parent=camera, position=(.10, -.25, .50), rotation=(90, 90, 0), origin_z=-.5, color=color.red,
+              on_cooldown=False)
+gun1.muzzle_flash = Entity(parent=gun1, world_scale=.5, model='quad', color=color.yellow, enabled=False)
+
 
 def update():
     if held_keys['left mouse']:
         shoot()
-    if held_keys['x']:
+    if held_keys['c']:
         player.scale = 1, 1, 1
         player.speed = 8
     else:
         player.scale = 2, 2, 2
         player.speed = 20
 
+
 def shoot():
     if not gun1.on_cooldown:
         print('pow pow')
         gun1.on_cooldown = True
         gun1.muzzle_flash.enabled = True
-        # from ursina.prefabs.ursfx import ursfx
-        # ursfx([(0.0, 0.0), (0.1, 0.9), (0.15, 0.75), (0.3, 0.14), (0.6, 0.0)], volume=0.5, wave='noise',
-        # pitch=random.uniform(-13, -12), pitch_change=-12, speed=3.0)
         invoke(gun1.muzzle_flash.disable, delay=.05)
         invoke(setattr, gun1, 'on_cooldown', False, delay=.50)
         print('reloading')
@@ -99,6 +95,7 @@ class FormerHuman(Entity):
 # Enemy()
 enemies = [FormerHuman(x=x * 4) for x in range(4)]
 
+
 def pause_input(key):
     if key == 'tab':  # press tab to toggle edit/play mode
         editor_camera.enabled = not editor_camera.enabled
@@ -113,6 +110,6 @@ def pause_input(key):
 
 pause_handler = Entity(ignore_paused=True, input=pause_input)
 
-Sky(texture='F_SKY1.png', filtering=False, color=color.red)
+Sky(texture='F_SKY1.png', color=color.red)
 
 app.run()

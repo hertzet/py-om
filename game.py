3b333a49807dd41f710e404e3af96a3eed9f1916
gato <leo@mis7vidas.com>
2022-07-07 18:41:01 -0300

    COMMIT 07/07/20
    Textures added to ALMOST the entire map
    Now the ASPECT RATIO is forced to be 1.230
    When C is hold your speed is lowered to 2
    Now you can run holding SHIFT (speed increases to 35)
    Jump Height now is 4

--- game.py
+++ game.py
@@ -1,33 +1,28 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
-# from ursina.shaders import lit_with_shadows_shader
-# from ursina.shaders import normals_shader
-# from ursina.shaders import unlit_shader
 from ursina.shaders import basic_lighting_shader
-Entity.default_shader = basic_lighting_shader
-# from ursina.shaders import matcap_shader - un re viaje
 
 random.seed(0)
 
 app = Ursina()
 window.borderless = False
 always_on_top = False
-window.title = 'py-doom'
+window.title = 'py-om'
 # window.fullscreen = True
-camera.fov = 90
-window.show_ursina_splash = True
+camera.fov = 30
+window.forced_aspect_ratio = 1.230
 
 editor_camera = EditorCamera(enabled=False, ignore_paused=True)
 
-MAP = Entity(model='E1M1_ME.obj', texture='2dassets/FLOOR7_1.PNG')
+MAP = Entity(model='e1m1-ver_9.blend', collider='mesh', shader=basic_lighting_shader, scale=(5), texture='2dassets/textures.png')
+
 # (model='e1m1.obj', texture='placeholder.png')
-MAP.collider = MeshCollider(MAP, center=(0, 0, 0))
-player = FirstPersonController(model='cube', texture='2dassets/py.png', speed=20)
+player = FirstPersonController(model='quad', z=-10, texture='2dassets/PLAYA1', origin_y=-.5, world_scale=2, jump_height=4)
 player.collider = BoxCollider(player, Vec3(0,1,0), Vec3(1,2,1))
 
 # position=(.10, -.25, .50), rotation=(90, 90, 0)
-gun = Entity(model='3dassets/gun1.obj', parent=camera, origin_z=-.5, color=color.gray, scale=(2, 2, 2), position=(-1, -.10, .20), rotation=(.30, 90, 0), 
-              on_cooldown=False)
+gun = Entity(load_texture('pistol.png'), parent=camera, on_cooldown=False, origin_z=-.5,)
+# origin_z=-.5, color=color.gray, scale=(2, 2, 2), position=(-1, -.10, .20), rotation=(.30, 90, 0)
 gun.muzzle_flash = Entity(parent=gun, world_scale=.5, model='quad', color=color.yellow, enabled=False, z=1)
 
 shootables_parent = Entity()
@@ -38,73 +33,14 @@ def update():
         shoot()
     if held_keys['c']:
         player.scale = 1, 1, 1
-        player.speed = 8
+        player.speed = 2
     else:
         player.scale = 2, 2, 2
         player.speed = 20
-
-from ursina.prefabs.health_bar import HealthBar
-
-# position=(5, 0, 0
-
-class bidon(Entity):
-    def __init__(self, **kwargs):
-        super().__init__(parent=shootables_parent, model='3dassets/bidon.obj', origin_y=-.5, texture='2dassets/bidon.png', collider='box', **kwargs) 
-        self.max_hp = 20
-        self.hp = self.max_hp
-
-    @property
-    def hp(self):
-        return self._hp
-
-    @hp.setter
-    def hp(self, value):
-        self._hp = value
-        if value <= 0:
-            destroy(self)
-            print("PLUUUUMM")
-            return
-
-class Enemy(Entity):
-    def __init__(self, **kwargs):
-        super().__init__(parent=shootables_parent, model='cube', scale_y=2, origin_y=-.5, texture='2dassets/FormerHumanSheet.png', collider='box', **kwargs)
-        self.health_bar = Entity(parent=self, y=1.2, model='cube', color=color.red, world_scale=(1.5,.1,.1))
-        self.max_hp = 50
-        self.hp = self.max_hp
-
-    def update(self):
-        dist = distance_xz(player.position, self.position)
-        if dist > 40:
-            return
-
-        self.health_bar.alpha = max(0, self.health_bar.alpha - time.dt)
-
-
-        self.look_at_2d(player.position, 'y')
-        hit_info = raycast(self.world_position + Vec3(0,1,0), self.forward, 30, ignore=(self,))
-        if hit_info.entity == player:
-            if dist > 2:
-                self.position += self.forward * time.dt * 5
-
-    @property
-    def hp(self):
-        return self._hp
-
-    @hp.setter
-    def hp(self, value):
-        self._hp = value
-        if value <= 0:
-            destroy(self)
-            print("mama i just killed a man!")
-            return
-
-        self.health_bar.world_scale_x = self.hp / self.max_hp * 1.5
-        self.health_bar.alpha = 1
-
-# Enemy()
-enemies = [Enemy(x=x*1) for x in range(1)]
-
-[bidon(x=x*4) for x in range(2)]
+    if held_keys['shift']:
+        player.speed = 35
+    else:
+        player.speed = 20
 
 def shoot():
     if not gun.on_cooldown:
@@ -133,7 +69,10 @@ def pause_input(key):
 
 pause_handler = Entity(ignore_paused=True, input=pause_input)
 
-Sky(texture='F_SKY1.png', color=color.red, filtering=True)
+Sky(texture='F_SKY1.png', color=color.red)
+pivot = Entity()
+# DirectionalLight(parent=pivot, y=2, z=3, shadows=True, rotation=(45, -45, 45), color=color.red)
+# AmbientLight(color=color.black10)
 
 app.run()
 
